8c577bb229a6ec6ed0afc742d8aae0204ae9e3e5
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-11-30 15:07:23 +0100

    Create 8_Venta.py

---
+++ pages/8_Venta.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.set_page_config(
+  page_title="Novus Mando",
+  page_icon="🎮",
+  layout="wide"
+)
+
+st.write("""
+#  Setas Meli 🚚 Supply Chain 🏭 VENTA
+""")
+
+st.sidebar.markdown("Desarrollado por Novus Mando, S.L. (www.novussolutions.io)")
